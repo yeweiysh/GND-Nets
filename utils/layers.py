ab5637e714c2be962a1c5d81600a2521b3516e48
Wei Ye <weiye@cs.ucsb.edu>
2022-01-13 11:42:31 +0800

    Update layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -84,6 +84,41 @@ def NeuralDiffMLP(inputs, output_dim, norm_mat, activation, in_drop=0.0):
         ret = tf.nn.bias_add(vals,bias)
 
         return activation(ret)  # activation
+    
+
+def NeuralDiffDS(inputs, output_dim, norm_mat, activation, in_drop=0.0):
+    with tf.name_scope('neural_diff_ds'):
+        if in_drop != 0.0:
+            inputs = tf.nn.dropout(inputs, 1.0 - in_drop)
+
+        inputs = tf.expand_dims(inputs, axis=0)
+        seq_fts = tf.layers.conv1d(inputs, output_dim, 1, use_bias=False)
+        if in_drop != 0.0:
+            seq_fts = tf.nn.dropout(seq_fts, 1.0 - in_drop)
+
+        hops = 2
+        H0 = tf.squeeze(seq_fts, axis=0)
+        subspace = list()
+        subspace.append(H0)
+
+        for i in range(hops-1):
+            H = subspace[-1]
+            H1 = tf.expand_dims(H, axis=0)
+            H1 = tf.layers.conv1d(H1, output_dim, 1, use_bias=False, activation="relu")
+            H1 = tf.layers.conv1d(H1, output_dim, 1, use_bias=False, activation="relu")
+            H1 = tf.squeeze(H1, axis=0)
+            for j in range(10):
+                H1 = tf.sparse_tensor_dense_matmul(norm_mat, H1)
+            Hi = H - H1
+            subspace.append(Hi)
+
+        vals = subspace[-1]
+        vals = tf.expand_dims(vals, axis=0)
+
+        bias = tf.get_variable(shape=[output_dim,],initializer=tf.initializers.zeros,name="bias")
+        ret = tf.nn.bias_add(vals,bias)
+
+        return activation(ret)  # activation
 
 
 def linear_layer(inputs, output_dim, activation, in_drop=0.0):
