3f5ca88853548c2f9292a3d4d1cbc4f6bf222ed7
Wei Ye <weiye@cs.ucsb.edu>
2022-01-17 10:50:09 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .gndnets_mlp import GNDNets_MLP
+from .gndnets_slp import GNDNets_SLP
+from .gndnets_ds import GNDNets_DS
---
+++ models/gndnets_slp.py
@@ -0,0 +1,21 @@
+import numpy as np
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+from utils import layers
+from models.base_gndnets import BaseGNDNets
+
+class GNDNets_SLP(BaseGNDNets):
+    def inference(inputs, nb_classes, training, ffd_drop,
+            norm_mat, output_dim, activation=tf.nn.elu):
+        
+        h_1 = layers.NeuralDiffSLP(inputs, norm_mat=norm_mat,
+            output_dim=output_dim, activation=activation,
+            in_drop=ffd_drop)
+        
+        logits = layers.linear_layer(h_1,
+            output_dim=nb_classes, activation=lambda x: x,
+            in_drop=ffd_drop)
+    
+        return logits
